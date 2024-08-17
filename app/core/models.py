682d3292779f5677aa800cc4b491b7b685ad03fe
MgK720 <igorhg720@gmail.com>
2024-08-18 00:28:15 +0200

    creating user_model #1

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,33 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)  # encrypting
+        user.save(using=self._db)  # self._db because it allows project to have multiple databases # noqa
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # Login with Django admin
+
+    objects = UserManager()  # assign UserManager
+
+    USERNAME_FIELD = 'email'  # this is how we change username -> email (basic model) # noqa
